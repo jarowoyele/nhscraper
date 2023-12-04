5d88c6f2e0f846e3ff42335eb0becb0ce596ae2c
jarowoyele <77020047+jarowoyele@users.noreply.github.com>
2023-12-04 09:36:59 +0100

    Update nhsscraper.py

--- nhsscraper.py
+++ nhsscraper.py
@@ -3,9 +3,19 @@ import cloudscraper
 import streamlit as st
 import pandas as pd
 
-def get_job_listings_url(position, location, miles, page):
-    URL = "https://www.jobs.nhs.uk/candidate/search/results?keyword={}&location={}&distance={}&skipPhraseSuggester=true&sort=publicationDateDesc&language=en&page={}"
-    return URL.format(position, location, miles, page)
+def get_job_listings_url(template, position, location, miles, page):
+    templates = {
+        "Newest Jobs": "https://www.jobs.nhs.uk/candidate/search/results?keyword={}&location={}&distance={}&skipPhraseSuggester=true&sort=publicationDateDesc&language=en&page={}",
+        "Best match": "https://www.jobs.nhs.uk/candidate/search/results?keyword={}&location={}&distance={}&skipPhraseSuggester=true&language=en&page={}"
+        # Add more templates as needed
+    }
+
+    template_url = templates.get(template)
+    if not template_url:
+        st.warning("Invalid template selected. Using default template.")
+        template_url = templates["Newest Jobs"]
+
+    return template_url.format(position, location, miles, page)
 
 def scrape_job_cards(soup, titles, linker):
     job_card = soup.find_all("li", {"class": "nhsuk-list-panel search-result nhsuk-u-padding-3"})
@@ -29,14 +39,14 @@ def scrape_job_details(linker, scraper, direct_links, cos):
         COS = soup2.find(attrs={'id': 'tier-two-sponsorship'})
         cos.append("Yes" if COS else "No")
 
-def scrape_jobs(position, location, miles, num_pages):
+def scrape_jobs(template, position, location, miles, num_pages):
     titles = []
     linker = []
     direct_links = []
     cos = []
 
     for page in range(1, num_pages + 1):
-        url = get_job_listings_url(position, location, miles, page)
+        url = get_job_listings_url(template, position, location, miles, page)
         scraper = cloudscraper.create_scraper()
         response = scraper.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -49,25 +59,28 @@ def scrape_jobs(position, location, miles, num_pages):
 def main():
     st.title("NHS Jobs Scraper App")
 
-    position = st.sidebar.text_input("Enter Position")
+    position = st.sidebar.text_input("Enter Job Title")
     location = st.sidebar.text_input("Enter Location")
     miles = st.sidebar.text_input("Enter Miles")
     num_pages = st.sidebar.number_input("Enter Number of Pages", min_value=1, max_value=10, value=1)
+    selected_templates = st.sidebar.multiselect("Select Templates", ["Newest Jobs", "Best match"], default=["Newest Jobs"])
 
     if st.sidebar.button("Scrape Jobs"):
         st.text("Scraping jobs... Please wait.")
-        titles, cos, direct_links = scrape_jobs(position, location, miles, num_pages)
+        titles, cos, direct_links = [], [], []
+
+        for template in selected_templates:
+            temp_titles, temp_cos, temp_direct_links = scrape_jobs(template, position, location, miles, num_pages)
+            titles.extend(temp_titles)
+            cos.extend(temp_cos)
+            direct_links.extend(temp_direct_links)
 
         data = {"Title": titles, "COS": cos, "Direct Link": direct_links}
-        
 
         st.write("## Job Results")
-        df = pd.DataFrame.from_dict(data,orient="index")
+        df = pd.DataFrame.from_dict(data, orient="index")
         df = df.transpose()
-        # df = df.sort_values("Closing Date",ascending=False)
         st.table(df)
-        
-        
 
 if __name__ == "__main__":
     main()
